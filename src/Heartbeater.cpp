627588ccf689af12b6355bdd2e79b28034431f04
Joe Khoury <joe.el.k97@gmail.com>
2017-11-28 17:40:37 +0100

    Remove code I committed by mistake.

--- src/Heartbeater.cpp
+++ src/Heartbeater.cpp
@@ -5,11 +5,6 @@ Heartbeater::Heartbeater (unsigned heartbeat_port) : heartbeat_port_(heartbeat_p
 {
 }
 
-void Heartbeater::SendHeartbeatsTo (const std::string& address)
-{
-  utils::ConnectSocketTo(heartbeat_socket_, address);
-}
-
 c::time_point<c::steady_clock> Now ()
 {
   return c::steady_clock::now();
