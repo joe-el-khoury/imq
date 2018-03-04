377f3796bf40a754b3a791ad653c77cceffce466
Joe Khoury <joe.el.k97@gmail.com>
2018-03-04 06:40:09 +0100

    Added timeouts to rpc responses.

--- src/rpc/RPCResponse.cpp
+++ src/rpc/RPCResponse.cpp
@@ -1,22 +1,23 @@
 #include "RPCResponse.hpp"
 
+#include <chrono>
+
 rpc::RPCResponse::RPCResponse (zmqpp::socket* socket) : socket_(socket)
 {
   received_.store(false);
-  did_callback_.store(false);
+  did_message_callback_.store(false);
   message_thread_ = new std::thread(&rpc::RPCResponse::CheckMessageReceipt, this);
 }
 
 rpc::RPCResponse::RPCResponse (const RPCResponse& other)
 {
   received_.store(other.received_.load());
-  received_message_ = other.received_message_.copy();
 
   socket_ = other.socket_;
   message_thread_ = other.message_thread_;
 
   message_callback_ = other.message_callback_;
-  did_callback_.store(other.did_callback_.load());
+  did_message_callback_.store(other.did_message_callback_.load());
 }
 
 rpc::RPCResponse::~RPCResponse ()
@@ -25,14 +26,14 @@ rpc::RPCResponse::~RPCResponse ()
   delete message_thread_;
 }
 
-void rpc::RPCResponse::DoCallback (zmqpp::message& message)
+void rpc::RPCResponse::DoMessageCallback (zmqpp::message& message)
 {
   if (!message_callback_) {
     // To avoid getting a bad_function error when calling the callback.
     return;
   }
   
-  if (did_callback_.load()) {
+  if (did_message_callback_.load()) {
     return;
   }
   
@@ -41,17 +42,47 @@ void rpc::RPCResponse::DoCallback (zmqpp::message& message)
 
   message_callback_(json::parse(message_str));
 
-  did_callback_.store(true);
+  did_message_callback_.store(true);
+}
+
+void rpc::RPCResponse::DoTimeoutCallback ()
+{
+  if (timeout_callback_) {
+    timeout_callback_();
+  }
+}
+
+unsigned rpc::RPCResponse::CurrentTime ()
+{
+  return std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::system_clock::now().time_since_epoch()
+  ).count();
+}
+
+bool rpc::RPCResponse::HasTimedOut ()
+{
+  if (!timeout_callback_set_) {
+    // Can't timeout if no timeout is set!
+    return false;
+  }
+  
+  unsigned now = CurrentTime();
+  return (now - start_time_) >= timeout_duration_;
 }
 
 void rpc::RPCResponse::CheckMessageReceipt ()
 {
+  zmqpp::message received_message;
   while (true) {
-    bool received = socket_->receive(received_message_);
+    bool received = socket_->receive(received_message, true);
     received_.store(received);
 
     if (received_.load()) {
-      DoCallback(received_message_);
+      DoMessageCallback(received_message);
+      return;
+    }
+    if (HasTimedOut()) {
+      DoTimeoutCallback();
       return;
     }
   }
@@ -60,7 +91,12 @@ void rpc::RPCResponse::CheckMessageReceipt ()
 void rpc::RPCResponse::OnMessageReceipt (std::function<void(const json&)> func)
 {
   message_callback_ = func;
-  if (received_.load()) {
-    DoCallback(received_message_);
-  }
+}
+
+void rpc::RPCResponse::OnTimeout (unsigned timeout, std::function<void()> func)
+{
+  start_time_ = CurrentTime();
+  timeout_duration_ = timeout;
+  timeout_callback_ = func;
+  timeout_callback_set_ = true;
 }
--- src/rpc/RPCResponse.hpp
+++ src/rpc/RPCResponse.hpp
@@ -16,15 +16,23 @@ private:
   using json = nlohmann::json;
   
   std::atomic<bool> received_;
-  zmqpp::message received_message_;
 
   zmqpp::socket* socket_;
   std::thread* message_thread_;
 
   std::function<void(const json&)> message_callback_;
-  std::atomic<bool> did_callback_;
+  std::atomic<bool> did_message_callback_;
+  void DoMessageCallback (zmqpp::message&);
+  
+  unsigned start_time_;
+  unsigned timeout_duration_ = 0;
+  unsigned CurrentTime ();
+  bool HasTimedOut ();
+  
+  std::function<void()> timeout_callback_;
+  bool timeout_callback_set_ = false;
+  void DoTimeoutCallback ();
 
-  void DoCallback (zmqpp::message&);
   void CheckMessageReceipt ();
 
 public:
@@ -34,6 +42,9 @@ public:
 
   // Register a callback for when a message is received.
   void OnMessageReceipt (std::function<void(const json&)>);
+
+  // Register a callback for when we time out.
+  void OnTimeout (unsigned, std::function<void()>);
 };
 
 }
