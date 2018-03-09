672728e10cbbf376545d44be717665e121aa1acd
Joe Khoury <joe.el.k97@gmail.com>
2018-03-09 04:13:03 +0100

    Implemented methods for RPC call.

--- src/rpc/RPCCall.cpp
+++ src/rpc/RPCCall.cpp
@@ -1,1 +1,33 @@
 #include "RPCCall.hpp"
+
+void rpc::RPCCall::OnMessageReceipt (MessageCallback message_receipt_callback)
+{
+  message_receipt_callback_ = message_receipt_callback;
+}
+
+void rpc::RPCCall::OnTimeout (unsigned timeout_duration, TimeoutCallback timeout_callback)
+{
+  timeout_duration_ = timeout_duration;
+  timeout_callback_ = timeout_callback;
+  timeout_callback_set_ = true;
+}
+
+rpc::RPCCall::MessageCallback rpc::RPCCall::GetMessageCallback ()
+{
+  return message_receipt_callback_;
+}
+
+rpc::RPCCall::TimeoutCallback rpc::RPCCall::GetTimeoutCallback ()
+{
+  return timeout_callback_;
+}
+
+unsigned rpc::RPCCall::GetTimeoutDuration ()
+{
+  return timeout_duration_;
+}
+
+bool rpc::RPCCall::TimeoutIsSet ()
+{
+  return timeout_callback_set_;
+}
--- src/rpc/RPCCall.hpp
+++ src/rpc/RPCCall.hpp
@@ -1,10 +1,33 @@
 #ifndef RPCCALL_HPP
 #define RPCCALL_HPP
 
+#include "../json.hpp"
+
 namespace rpc {
 
 class RPCCall
 {
+private:
+  using json = nlohmann::json;
+
+  using MessageCallback = std::function<void(const json&)>;
+  using TimeoutCallback = std::function<void()>;
+
+  MessageCallback message_receipt_callback_;
+  
+  unsigned timeout_duration_ = 0;
+  bool timeout_callback_set_ = false;
+  TimeoutCallback timeout_callback_;
+
+public:
+  void OnMessageReceipt (MessageCallback);
+  void OnTimeout (unsigned, TimeoutCallback);
+
+  MessageCallback GetMessageCallback ();
+  TimeoutCallback GetTimeoutCallback ();
+
+  unsigned GetTimeoutDuration ();
+  bool TimeoutIsSet ();
 };
 
 } // namespace rpc
