d6e8d4925266167d1d3370b3901dab99f3f1ef46
Joe Khoury <joe.el.k97@gmail.com>
2018-03-13 12:18:54 +0100

    Data structure change for cluster.

--- src/Cluster.cpp
+++ src/Cluster.cpp
@@ -11,15 +11,20 @@ Cluster& Cluster::GetInstance ()
 
 bool Cluster::NodeInCluster (const std::string& hostname, unsigned port)
 {
-  return (nodes_.find(hostname) != nodes_.end());
+  HostAndPort host_and_port(hostname, port);
+  bool in_vector = (std::find(host_and_ports_.begin(), host_and_ports_.end(), host_and_port) != host_and_ports_.end());
+  bool in_map = nodes_.find(host_and_port) != nodes_.end();
+  
+  return in_vector && in_map;
 }
 
 void Cluster::AddNode (const std::string& hostname, unsigned port)
 {
+  HostAndPort host_and_port(hostname, port);
   if (NodeInCluster(hostname, port)) {
-    // We should just overwrite. This'll probably never happen though.
-    nodes_[hostname] = port;
+    nodes_[host_and_port] = nullptr;
   } else {
-    nodes_.insert({hostname, port});
+    // nullptr because we will lazy-load the rpc clients if needed.
+    nodes_.insert({{hostname, port}, nullptr});
   }
 }
--- src/Cluster.hpp
+++ src/Cluster.hpp
@@ -2,13 +2,49 @@
 #define CLUSTER_HPP
 
 #include <unordered_map>
+#include <vector>
+
+#include "rpc/RPCClient.hpp"
+
+struct HostAndPort
+{
+  std::string host;
+  unsigned port;
+
+  HostAndPort (const std::string& host_, unsigned port_)
+  {
+    host = host_;
+    port = port_;
+  }
+
+  bool operator== (const HostAndPort& other) const
+  {
+    return host == other.host && port == other.port;
+  }
+};
+namespace std
+{
+template<>
+struct hash<HostAndPort>
+{
+  std::size_t operator() (const HostAndPort& host_and_port) const
+  {
+    const std::string& host = host_and_port.host;
+    unsigned port = host_and_port.port;
+    
+    return std::hash<std::string>()(host + std::to_string(port));
+  }
+};
+}
 
 class Cluster
 {
 private:
   // Store the nodes in this map.
-  // Key is the hostname, value is the port.
-  std::unordered_map<std::string, unsigned> nodes_;
+  // Key is host and port, value is the rpc client connected to that node.
+  std::unordered_map<HostAndPort, rpc::RPCClient*> nodes_;
+  // To quickly get the nodes in the cluster.
+  std::vector<HostAndPort> host_and_ports_;
 
   Cluster ();
 
