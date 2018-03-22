350ca6fc99f7b14456cb27c253bfa77de6340c55
Joe Khoury <joe.el.k97@gmail.com>
2018-03-22 05:09:39 +0100

    Made RPCServer member a shared_ptr instead of a raw pointer.

--- src/IMQNode.cpp
+++ src/IMQNode.cpp
@@ -9,18 +9,19 @@
 
 #include "rpc/RPCCall.hpp"
 
-IMQNode::IMQNode (const std::string& host, unsigned server_port)
+IMQNode::IMQNode (const std::string& host, unsigned server_port, bool first_node_in_cluster)
 {
-  rpc_server_ = new rpc::RPCServer(server_port);
+  rpc_server_ = std::make_shared<rpc::RPCServer>(host, server_port);
 
   cluster = &Cluster::GetInstance();
+  if (first_node_in_cluster) {
+    cluster->AddNode(host, server_port);
+  }
 }
 
-IMQNode::~IMQNode ()
+void IMQNode::BootstrapCluster (
+    const std::string& cluster_node_hostname, unsigned cluster_node_port)
 {
-  if (rpc_server_) {
-    delete rpc_server_;
-  }
 }
 
 void IMQNode::Run ()
--- src/IMQNode.hpp
+++ src/IMQNode.hpp
@@ -7,20 +7,25 @@
 #include <atomic>
 #include <string>
 
+#include <memory>
+
 #include "rpc/RPCServer.hpp"
+#include "rpc/RPCClient.hpp"
 
 #include "Cluster.hpp"
 
 class IMQNode
 {
 private:
-  rpc::RPCServer* rpc_server_;
-
+  std::shared_ptr<rpc::RPCServer> rpc_server_;
+  
   Cluster* cluster;
 
 public:
-  IMQNode (const std::string&, unsigned);
-  ~IMQNode ();
+  IMQNode (const std::string&, unsigned, bool);
+
+  // Bootstrapping means getting the cluster up and running.
+  void BootstrapCluster (const std::string&, unsigned);
 
   void Run ();
 };
