f2ca88eefd32a70f886f066ddfc9a5841415f9bb
Joe Khoury <joe.el.k97@gmail.com>
2018-04-19 01:40:22 +0200

    A meta store to hold info about the current node.

---
+++ src/meta/MetaStore.cpp
@@ -0,0 +1,5 @@
+#include "meta/MetaStore.hpp"
+
+#include "utils/HostAndPort.hpp"
+
+HostAndPort MetaStore::my_host_and_port_;
---
+++ src/meta/MetaStore.hpp
@@ -0,0 +1,19 @@
+#ifndef METASTORE_HPP
+#define METASTORE_HPP
+
+#include "utils/HostAndPort.hpp"
+
+class MetaStore
+{
+private:
+  static HostAndPort my_host_and_port_;
+public:
+  static void SetHost (const std::string& host) { MetaStore::my_host_and_port_.host = host; }
+  static void SetPort (unsigned port) { MetaStore::my_host_and_port_.port = port; }
+
+  static const HostAndPort& GetHostAndPort () { return MetaStore::my_host_and_port_; }
+  static const std::string& GetHost () { return MetaStore::GetHostAndPort().host; }
+  static unsigned GetPort () { return MetaStore::GetHostAndPort().port; }
+};
+
+#endif // METASTORE_HPP
