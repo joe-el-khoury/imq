063796199aac0b27c40cc28a2b5e9729d3e87606
Joe Khoury <joe.el.k97@gmail.com>
2018-06-12 09:07:22 +0200

    Added method to remove node from cluster.

--- src/health/Health.hpp
+++ src/health/Health.hpp
@@ -18,26 +18,23 @@ class Health
 private:
   using json = nlohmann::json;
 
-  // To get nodes in cluster.
-  Cluster* cluster_ = &Cluster::GetInstance();
-
   // How many times to keep checking on a node before marking it fully failed.
   const int REMOVE_AFTER_NUM_RETRIES = 3;
   
   // Map from the address of the potentially failed node and the number of retries.
   std::unordered_map<utils::HostAndPort, int> potentially_failed_nodes_and_num_retires_;
-  std::mutex map_mutex;
+  std::mutex map_mutex_;
 
   void MarkAsPotentiallyFailing (const std::string& host, unsigned port)
   {
-    std::lock_guard<std::mutex> lk(map_mutex);
+    std::lock_guard<std::mutex> lk(map_mutex_);
     
     utils::HostAndPort hap(host, port);
     potentially_failed_nodes_and_num_retires_.insert({hap, 0});
   }
-  void UnmarkAsPotentiallyFailing (const std::string& host, unsigned port)
+  void RemoveNodeFromPotentiallyFailingMap (const std::string& host, unsigned port)
   {
-    std::lock_guard<std::mutex> lk(map_mutex);
+    std::lock_guard<std::mutex> lk(map_mutex_);
     
     utils::HostAndPort hap(host, port);
     auto iter = potentially_failed_nodes_and_num_retires_.find(hap);
@@ -45,9 +42,13 @@ private:
       potentially_failed_nodes_and_num_retires_.erase(iter);
     }
   }
+  void UnmarkAsPotentiallyFailing (const std::string& host, unsigned port)
+  {
+    RemoveNodeFromPotentiallyFailingMap(host, port);
+  }
   bool IsPotentiallyFailing (const std::string& host, unsigned port)
   {
-    std::lock_guard<std::mutex> lk(map_mutex);
+    std::lock_guard<std::mutex> lk(map_mutex_);
     
     utils::HostAndPort hap(host, port);
     return potentially_failed_nodes_and_num_retires_.find(hap) 
@@ -55,7 +56,7 @@ private:
   }
   unsigned GetNumberOfRetriesForNode (const std::string& host, unsigned port)
   {
-    std::lock_guard<std::mutex> lk(map_mutex);
+    std::lock_guard<std::mutex> lk(map_mutex_);
     
     utils::HostAndPort hap(host, port);
     if (IsPotentiallyFailing(host, port)) {
@@ -66,10 +67,20 @@ private:
   }
   void IncrementNumberOfRetriesForNode (const std::string& host, unsigned port)
   {
-    std::lock_guard<std::mutex> lk(map_mutex);
+    std::lock_guard<std::mutex> lk(map_mutex_);
     
     utils::HostAndPort hap(host, port);
     potentially_failed_nodes_and_num_retires_[hap]++;
+  }
+
+  Cluster* cluster_ = &Cluster::GetInstance();
+
+  void RemoveNodeFromCluster (const std::string& host, unsigned port)
+  {
+    cluster_->RemoveNode(host, port);
+
+    // We can remove the node from the potentially failing map, because it's fully failed.
+    RemoveNodeFromPotentiallyFailingMap(host, port);
   }
   
   unsigned GetCurrentTime ();
