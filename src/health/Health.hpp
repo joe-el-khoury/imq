7265c100ab92404f0c1dd8b2b1cf289a0a101e3b
Joe Khoury <joe.el.k97@gmail.com>
2018-06-12 08:42:04 +0200

    Added map to track potentially failing nodes.

--- src/health/Health.hpp
+++ src/health/Health.hpp
@@ -9,6 +9,7 @@
 
 #include <thread>
 #include <atomic>
+#include <unordered_set>
 
 class Health
 {
@@ -17,6 +18,31 @@ private:
 
   // To get nodes in cluster.
   Cluster* cluster_ = &Cluster::GetInstance();
+
+  // How many times to keep checking on a node before marking it fully failed.
+  const int REMOVE_AFTER_NUM_RETRIES = 3;
+  
+  // Map from the address of the potentially failed node and the number of retries.
+  std::unordered_map<utils::HostAndPort, int> potentially_failed_nodes_and_num_retires_;
+
+  bool IsPotentiallyFailing (const std::string& host, unsigned port)
+  {
+    utils::HostAndPort hap(host, port);
+    return potentially_failed_nodes_and_num_retires_.find(hap) 
+      != potentially_failed_nodes_and_num_retires_.end();
+  }
+  unsigned GetNumberOfRetriesForNode (const std::string& host, unsigned port)
+  {
+    utils::HostAndPort hap(host, port);
+    if (IsPotentiallyFailing(host, port)) {
+      return potentially_failed_nodes_and_num_retires_[hap];
+    }
+  }
+  void IncrementNumberOfRetriesForNode (const std::string& host, unsigned port)
+  {
+    utils::HostAndPort hap(host, port);
+    potentially_failed_nodes_and_num_retires_[hap]++;
+  }
   
   unsigned GetCurrentTime ();
 
